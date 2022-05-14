5102444413057c662e103e6f3410232b3f145d74
anousone <anousone.litd@gmail.com>
2022-05-14 22:39:28 +0700

    clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import './styles.css'
+import Img from './rule2.jpeg'
 export const App = () => {
     return (
         <>
             <h1>Hello react template</h1>
+            <img src={Img} alt="my rule" width="300" height="300" />
         </>
     )
 }
