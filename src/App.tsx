5cbe330eee92ecae19caad4a53366a1e0b4ff527
anousone <anousone.litd@gmail.com>
2022-05-15 07:26:24 +0700

    EP:3 config env:dev & prod in webpack

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Img from './rule2.jpeg'
 export const App = () => {
     return (
         <>
-            <h1>Hello react template</h1>
+            <h1>
+                Hello react template {process.env.NODE_ENV} : {process.env.name}
+            </h1>
             <img src={Img} alt="my rule" width="300" height="300" />
         </>
     )
---
+++ webpack/webpack.common.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    output: {
+        path: path.resolve(__dirname, '..', './build'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '..', './src/index.html'),
+        }),
+    ],
+}
