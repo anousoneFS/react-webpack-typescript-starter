982a847f13eaa5d6028353f7a3197e40f91356ff
anousone <anousone.litd@gmail.com>
2022-05-15 11:06:51 +0700

    error

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Img from './rule2.jpeg'
 import Counter from './Counter'
 
 export const App = (): ReactElement => {
+  const myname = 'anousone'
   return (
     <>
       <h1>
-        Edited react template {process.env.NODE_ENV} : {process.env.name}
+        Edited react template {process.env.NODE_ENV} : {process.env.name}:{' '}
+        {myname}
       </h1>
       <img src={Img} alt="my rule" width="300" height="300" />
       <Counter />
